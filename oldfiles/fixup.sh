a7b326b524c71b5fd83cbddb61607553a94fb1ff
Joe Mueller <joseph.e.mueller@gmail.com>
2023-02-16 21:30:00 -0700

    Typo in  script

--- oldfiles/fixup.sh
+++ oldfiles/fixup.sh
@@ -7,7 +7,7 @@
 # apply pandoc to each aspx file, generation markdown
 find \*.aspx > $$files
 for file in `cat $$file` do
-    basename -s aspx file > $basename
+    basename -s .aspx $file > $basename
     $destination = $basename.markdown
     pandoc --from=html --to=markdown < $file > $destination
 done
