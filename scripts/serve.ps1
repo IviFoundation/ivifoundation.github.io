fca40189abd995f69bef986b2a0b310096a2c74d
Alejandro Barreto <alejandro.barreto@ni.com>
2023-02-27 14:55:29 -0600

    Profile why the site is so slow

--- scripts/serve.ps1
+++ scripts/serve.ps1
@@ -13,5 +13,6 @@ docker run --rm `
     --name="ivi-foundation-website" `
     --publish="4000:4000" `
     --volume="$root/site:/srv/jekyll:Z" `
+    --env=JEKYLL_LOG_LEVEL=debug `
     --rm --interactive --tty jekyll/jekyll `
-    jekyll serve --incremental --watch --force_polling --host 0.0.0.0 --trace @jekyllArgs
+    jekyll serve --incremental --watch --force_polling --host 0.0.0.0 --profile --trace @jekyllArgs
